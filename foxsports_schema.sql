d6f289a7acacc8f77f8972cc7d19f005d19bfb85
Nate Chamness <nathaniel.chamness@gmail.com>
2014-11-25 15:17:46 -0800

    almost done parsing foxsports db

--- foxsports_schema.sql
+++ foxsports_schema.sql
@@ -1,3 +1,5 @@
+drop table if exists play_by_play;
+
 create table play_by_play (
   id integer not null primary key autoincrement,
   game integer references game(id),
@@ -6,26 +8,35 @@ create table play_by_play (
   seconds integer,
   home_score integer,
   away_score integer,
-  team integer,
-  textual_description text,
-  play_type text check( play_type in ('R', 'B', 'S', 'A', 'O') ) not null default 'O',
+  team text,
+  description text,
+  play_type text default 'other', 
   primary_player text,
+  secondary_player,
+  shot_type text,
+  shot_made text,
+  shot_distance integer,
   x_coord real,
-  y_coord real
+  y_coord real,
+  rebound_type,
+  foul_type,
+  timeout_type,
+  turnover_type
 );
 
-create table player (
-  id integer not null primary key,
-  first_name not null text,
-  last_name not null text,
-  team integer not null references team(id)
-);
-
+/*
 create table team (
   id integer not null primary key,
   city text not null,
   name text not null,
   division not null integer
+);
+
+create table player (
+  id integer,
+  first_name text,
+  last_name not null text,
+  team integer not null references team(id)
 );
 
 create table game (
@@ -37,6 +48,7 @@ create table game (
   away_score integer not null
 );
 
+
 create table division (
   id integer not null primary key,
   name text not null,
@@ -47,3 +59,4 @@ create table conference (
   id integer not null primary key,
   name text not null
 );
+*/
--- scrape_foxsports.py
+++ scrape_foxsports.py
@@ -70,27 +70,195 @@ class Play:
     def __repr__(self):
         return 'quarter: ' + self.quarter + ' minutes: ' + self.minutes + ' seconds: ' + self.seconds + ' home_score: ' + self.home_score + ' away_score: ' + self.away_score + '\n\ttextual_description: ' + self.textual_description 
 
+def insert_play(description):
+    play = {}
+    play['play_type'] = play_type(description.lower())
+
+
+    if play['play_type'] == 'shot' or play['play_type'] == 'make' or play['play_type'] == 'miss':
+        shot = re.search('(.*) (makes|misses) an? (.*) from (.*) (feet|foot) out\.', description)
+        shot2 = re.search('(.*) (makes|misses) an? (.*)\.', description)
+        dunk = re.search('(.*) dunks\.', description)
+        dunks_from = re.search('(.*) dunks from (\d) (foot|feet) out\.', description)
+        if shot:
+            play['primary_player'] = shot.group(1)
+            play['shot_made'] = shot.group(2)
+            play['shot_type'] = shot.group(3).strip('shot').strip()
+            play['shot_distance'] = shot.group(4)
+        elif shot2:
+            play['primary_player'] = shot2.group(1)
+            play['shot_made'] = shot2.group(2)
+            play['shot_type'] = shot2.group(3).strip('shot').strip()
+        elif dunk:
+            play['primary_player'] = dunk.group(1)
+            play['shot_type'] = 'dunk'
+            play['shot_made'] = 'made'
+        elif dunks_from:
+            play['primary_player'] = dunks_from.group(1)
+            play['shot_type'] = 'dunk'
+            play['shot_made'] = 'made'
+            play['shot_distance'] = dunks_from.group(2)
+        else:
+            print(description)
+    elif play['play_type'] == 'rebound':
+        rebound = re.search('(.*) with an? (offensive|defensive) rebound\.', description)
+        play['primary_player'] = rebound.group(1)
+        play['rebound_type'] = rebound.group(2)
+    elif play['play_type'] == 'foul':
+        foul = re.search('(.*) foul committed by (.*)\.', description)
+        if foul:
+            play['foul_type'] = foul.group(1)
+            play['primary_player'] = foul.group(2)
+        else:
+            print(description)
+    elif play['play_type'] == 'timeout':
+        timeout = re.search('(.*) take a (.*) timeout\.', description)
+        official = re.search("Official's timeout\.", description)
+        if timeout:
+            play['team'] = timeout.group(1)
+            play['timeout_type'] = timeout.group(2)
+        elif official:
+            play['timeout_type'] = 'official'
+        else:
+            print(description)
+    elif play['play_type'] == 'substitution':
+        sub = re.search('Substitution: (.*) in for (.*)\.', description)
+        play['primary_player'] = sub.group(1)
+        play['secondary_player'] = sub.group(2)
+    elif play['play_type'] == 'free throw':
+        free = re.search('(.*) (makes|misses) free throw (\d) of (\d)\.', description)
+        technical = re.search('(.*) (makes|misses) free throw technical\.', description)
+        special = re.search('(.*) (makes|misses) free throw (.*) (\d) of (\d)\.', description)
+        if free:
+            play['primary_player'] = free.group(1)
+            play['shot_made'] = free.group(2)
+        elif technical:
+            play['primary_player'] = technical.group(1)
+            play['shot_made'] = technical.group(2)
+        elif special:
+            play['primary_player'] = special.group(1)
+            play['shot_made'] = special.group(2)
+            play['shot_type'] = special.group(3)
+        else:
+            print(description)
+        play['shot_type'] = 'free throw'
+    elif play['play_type'] == 'steal':
+        steal = re.search('(.*) steals the ball from (.*)\.', description)
+        play['primary_player'] = steal.group(1)
+        play['secondary_player'] = steal.group(2)
+    elif play['play_type'] == 'turnover' or play['play_type'] == 'kick' or play['play_type'] == 'delay of game':
+        turnover = re.search('(.*) is charged with a turnover due to a (.*)\.', description)
+        turnover2 = re.search('(.*) with a (.*) turnover', description)
+        shot_clock = re.search('(.*) with a turnover: (.*)', description)
+        kick = re.search('(.*) kicks the ball\. (.*) ball\.', description)
+        delay = re.search('(.*) commit a delay of game violation\. (.*) ball\.', description)
+        if turnover:
+            play['primary_player'] = turnover.group(1)
+        elif turnover2:
+            play['primary_player'] = turnover2.group(1)
+            play['turnover_type'] = turnover2.group(2)
+        elif shot_clock:
+            play['team'] = shot_clock.group(1)
+            play['turnover_type'] = shot_clock.group(2)
+        elif kick:
+            play['primary_player'] = kick.group(1)
+            play['turnover_type'] = 'kick'
+        elif delay:
+            play['team'] = delay.group(1)
+            play['turnover_type'] = 'delay of game'
+        else:
+            print(description)
+    elif play['play_type'] == 'blocks':
+        block = re.search("(.*) blocks a ([A-Z][\w'\.-]+ [A-Z][\w'\.-]+) .*\.", description)
+        play['primary_player'] = block.group(1)
+        play['secondary_player'] = block.group(2)
+    elif play['play_type'] == 'jump ball':
+        jump_ball = re.search('Jump Ball: (.*) vs\. (.*) -- (.*) gains possession\.', description)
+        if jump_ball:
+            play['primary_player'] = jump_ball.group(3)
+    elif play['play_type'] == 'assist':
+        assist = re.search('(.*) makes an? (.*)\. (.*) with the assist\.', description)
+        dunk = re.search('(.*) dunks from (\d) (foot|feet) out. (.*) with the assist\.', description)
+        other_dunk = re.search('(.*) dunks\. (.*) with the assist\.', description)
+        if assist:
+            play['primary_player'] = assist.group(1)
+            play['shot_type'] = assist.group(2)
+            play['secondary_player'] = assist.group(3)
+        elif dunk:
+            play['primary_player'] = dunk.group(1)
+            play['shot_type'] = 'dunk'
+            play['shot_distance'] = dunk.group(2) 
+            play['secondary_player'] = dunk.group(4)
+        elif other_dunk:
+            play['primary_player'] = other_dunk.group(1)
+            play['shot_type'] = 'dunk'
+            play['secondary_player'] = other_dunk.group(2)
+        else:
+            print(description)
+        
+    elif play['play_type'] != 'start' and play['play_type'] != 'end':
+        print(description)
+
+    return play 
+
+def play_type(play_description):
+    keywords = 'blocks|rebound|substitution|foul|timeout|free throw|steal|turnover|start|end|jump ball|assist|kick|delay of game'
+    shot = 'shot|make|miss|dunk'
+    
+    result = re.search(keywords, play_description)
+
+    if result:
+        return result.group(0)
+    elif re.search(shot, play_description):
+        return 'shot'
+    else:
+        return ''
+
+def get_value(play, column):
+    if column in play:
+        return play[column]
+    else:
+        return None
+
+def get_column_name(key):
+    columns = {'quarter': 'quarter', 
+               'minutes': 'time-minutes', 
+               'seconds': 'time-seconds',
+               'home_score': 'home-score',
+               'away_score': 'visitor-score',
+               'team': 'team',
+               'description': 'textual-description',
+               'play_type': 'play_type',
+               'primary_player': 'primary_player', 
+               'shot_type': 'shot_type',
+               'shot_made': 'shot_made',
+               'shot_distance': 'shot_distance',
+               'x_coord': 'x-shot-coord',
+               'y_coord': 'y-shot-coord',
+               'timeout_type': 'timeout_type',
+               'foul_type': 'foul_type',
+               'rebound_type': 'rebound_type',
+               'secondary_player': 'secondary_player',
+               'turnover_type': 'turnover_type'
+            }
+    return columns[key]
+
 def insert_into_db(data):
     with sqlite3.connect('foxsports.db') as conn:
         for play in data['pbp']['sports-scores']['nba-scores']['nba-playbyplay']['play']:
-            conn.execute('INSERT INTO play_by_play (quarter, minutes, seconds, home_score, away_score, textual_description, play_type, x_coord, y_coord) VALUES (?,?,?,?,?,?,?,?)', 
-                    [play['quarter'], 
-                     play['time-minutes'], 
-                     play['time-seconds'], 
-                     play['home-score'], 
-                     play['visitor-score'],
-                     play['textual-description'],
-                     play_type(play['textual-description']),
-                     play['x-shot-coord'],
-                     play['y-shot-coord']])
 
-def play_type(play_description):
-    return 'Rebound'
+            insert = insert_play(play['textual-description'])
+            insert.update(play)
+            columns = ['quarter', 'minutes', 'seconds', 'home_score', 'away_score', 'description', 'play_type', 'primary_player', 'secondary_player', 'shot_made', 'shot_distance', 'x_coord', 'y_coord', 'timeout_type', 'foul_type', 'rebound_type', 'shot_type', 'turnover_type']
+            insert_data = [get_value(insert, get_column_name(d)) for d in columns]
+
+            conn.execute('INSERT INTO play_by_play (' + ','.join(columns) + ') VALUES (' + ','.join(['?'] * len(columns)) + ')', insert_data)
+
     
 
 def parse(games_dir):
     for f in glob.glob(os.path.join(games_dir, '*.html')):
-        print(f)
+        #print(f)
         with open(f, errors='ignore') as fi:
             soup = BeautifulSoup(fi)
             script = soup.find('script', text=re.compile('nba\.initialUpdate'))
@@ -120,3 +288,17 @@ if __name__ == '__main__':
     #    urllib.request.urlretrieve(base_url + game_url, games_dir + g.game_date + '_' + g.away_team + '@' + g.home_team + '.html')
     parse(games_dir)
 
+"""
+[play['quarter'], 
+ play['time-minutes'], 
+ play['time-seconds'], 
+ play['home-score'], 
+ play['visitor-score'],
+ play['textual-description'],
+ play_type,
+ player,
+ made,
+ distance,
+ play['x-shot-coord'],
+ play['y-shot-coord']])
+"""
